d4e6d5f1c11fcca5769a353b96114fae9c5e5ed2
pascallino <luvpascal.ojukwu@yahoo.com>
2023-06-06 05:09:04 -0700

    low

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def islower(c):
-    if len(c) == 0:
+    if c == "":
+        return False
+    elif len(c) == 0:
         return False
     elif (c >= 'a' and c <= 'z'):
         return True
