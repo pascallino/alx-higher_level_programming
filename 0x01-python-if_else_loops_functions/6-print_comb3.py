d12b60a6550e6977f200edb1d49a5fd378deeb19
pascallino <luvpascal.ojukwu@yahoo.com>
2023-06-06 05:20:36 -0700

    new comb

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-for i in range(0, 10) :
-    for j in range(0, 10) :
-        if i < j :
-            if i == 8 and j == 9 :
+for i in range(0, 10):
+    for j in range(0, 10):
+        if i < j:
+            if i == 8 and j == 9:
                 print(f"{i:d}{j:d}")
-            else :
-                  print(f"{i:d}{j:d}, ", end="")
+            else:
+                print(f"{i:d}{j:d}, ", end="")
