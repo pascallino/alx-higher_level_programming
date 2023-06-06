b64bf4538a51e68a3835cc00778554ea86c12fa1
pascallino <luvpascal.ojukwu@yahoo.com>
2023-06-06 04:42:15 -0700

    exec last 1

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,8 +1,8 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 def print_last_digit(number):
-    if number < 0 :
+    if number < 0:
         print_last_digit = abs(number) % 10
-    else :
+    else:
         print_last_digit = number % 10
     print("{}".format(print_last_digit), end="")
     return print_last_digit
