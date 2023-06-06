c36b6341760aba7ab74170eb429f37a2d511dace
pascallino <luvpascal.ojukwu@yahoo.com>
2023-06-06 05:30:24 -0700

    new comb1

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
-for i in range(00, 100) :
-   #for j in range o 0, 10) :
-      # if i < 9 or j < 9 :
-    if i < 99 :
-        print(f"{i:02d}, ", end="")
-    elif i == 99 :
-            print(f"{i:d}")
+for i in range(00, 100):
+    if i < 99:
+        print("{:02d}, ".format(i), end="")
+    elif i == 99:
+        print("{:d}".format(i))
