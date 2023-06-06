02f6248d9e1faa6919665a6f8a6e6ad950d6ceb1
pascallino <luvpascal.ojukwu@yahoo.com>
2023-06-06 05:40:30 -0700

    alpha

--- 0x01-python-if_else_loops_functions/3-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabet.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
-i = 97
-while i <= 122:
+for i in range(97, 123):
     if i == 113 or i == 101:
-        i += 1
         continue
     print("{}".format(chr(i)), end="")
-    i += 1
