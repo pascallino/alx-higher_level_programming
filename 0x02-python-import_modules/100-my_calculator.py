f4079f7f6bb5f74d12caf1438bc84ad14a3fa287
pascallino <luvpascal.ojukwu@yahoo.com>
2023-06-08 04:54:44 -0700

    edited

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -3,18 +3,18 @@ import sys
 
 from builtins import IndexError
 
-
-opt = ['+', '-', '*', '/']
-if not len(sys.argv) >= 4:
-    print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-    sys.exit(1)
-elif len(sys.argv) >= 4:
-    if not sys.argv[2] in opt:
-        print("Unknown operator. Available operators: +, -, * and /")
+if __name__ == '__main__':
+    opt = ['+', '-', '*', '/']
+    if not len(sys.argv) >= 4:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
-a = int(sys.argv[1])
-b = int(sys.argv[3])
-ch = sys.argv[2]
-exp = f"{a} {ch} {b}"
-res = eval(exp)
-print("{} {} {} = {}".format(a, ch, b, res))
+    elif len(sys.argv) >= 4:
+        if not sys.argv[2] in opt:
+            print("Unknown operator. Available operators: +, -, * and /")
+            sys.exit(1)
+    a = int(sys.argv[1])
+    b = int(sys.argv[3])
+    ch = sys.argv[2]
+    exp = f"{a} {ch} {b}"
+    res = eval(exp)
+    print("{} {} {} = {}".format(a, ch, b, res))
