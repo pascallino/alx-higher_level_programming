aecc15f69436f23e0ae5667ddcf74bd01fa3e71f
pascallino <luvpascal.ojukwu@yahoo.com>
2023-06-08 02:40:32 -0700

    update task 5

--- 0x02-python-import_modules/5-variable_load.py
+++ 0x02-python-import_modules/5-variable_load.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import variable_load_5
-
+#   from variable_load_5 import a
+#    print(a);
     value = variable_load_5.__dict__['a']
     print("{}".format(value))
