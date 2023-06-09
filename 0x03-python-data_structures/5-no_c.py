02523f0737c34f50bfe4c1fc80f8a4a922384528
pascallino <luvpascal.ojukwu@yahoo.com>
2023-06-09 01:52:41 -0700

    no c

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+def no_c(my_string):
+    str = ""
+    for ch in my_string:
+        if ch == 'c' or ch == 'C':
+            continue
+        str = str + ch
+    return str
+            
