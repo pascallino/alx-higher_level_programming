ee3de15ad53c1e13603a45b02f54bdaa1561a2bc
pascallino <luvpascal.ojukwu@yahoo.com>
2023-06-14 05:22:39 -0700

    number keys

--- 0x04-python-more_data_structures/5-number_keys.py
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -1,8 +1,4 @@
 #!/usr/bin/python3
 def number_keys(a_dictionary):
-    if a_dictionary:
-        # len_1 = len(a_dictionary.keys())
-        len_1 = 0
-        for key, value in a_dictionary.items():
-            len_1 += 1
-        return len_1
+    if a_dictionary is not None:
+        return len(a_dictionary.keys())
