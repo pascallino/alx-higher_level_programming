6a546f69f4f86b06d488a10161209628b95234fb
pascallino <luvpascal.ojukwu@yahoo.com>
2023-06-14 05:29:30 -0700

    4-only_diff_elements.py

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
 def only_diff_elements(set_1, set_2):
-    if not set_1 or not set_2:
-        return
-    else:
-        common_elements = set()
-        # common_elements = set_1.intersection(set_2)
-        # common_elements = set_1 && set_2
-        common_elements = set_1 ^ set_2
-        return common_elements
+    common_elements = set()
+    # common_elements = set_1.intersection(set_2)
+    # common_elements = set_1 && set_2
+    common_elements = set_1 ^ set_2
+    return common_elements
