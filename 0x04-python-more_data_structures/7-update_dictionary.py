903aa551ce16bc98cf3f9e45d4747d28eb4de203
pascallino <luvpascal.ojukwu@yahoo.com>
2023-06-14 05:34:38 -0700

    7-update_dictionary.p.py

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 def update_dictionary(a_dictionary, key, value):
-    if len(a_dictionary.keys()) > 0:
-        if key in a_dictionary:
-            a_dictionary[key] = value
-            return a_dictionary
-        else:
-            new_entry = {key: value}
-            a_dictionary.update(new_entry)
-            return a_dictionary
+    if key in a_dictionary:
+        a_dictionary[key] = value
+        return a_dictionary
+    elif isinstance(key, str) and not len(key) == 0:
+        new_entry = {key: value}
+        a_dictionary.update(new_entry)
+        return a_dictionary
