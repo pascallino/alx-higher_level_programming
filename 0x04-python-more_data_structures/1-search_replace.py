2b5245bd5412c7cba03d5223c9f31c11154a41fb
pascallino <luvpascal.ojukwu@yahoo.com>
2023-06-14 05:48:27 -0700

    search

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    if my_list:
-        lst = []
-        for ch in my_list:
-            if ch == search:
-                lst.append(replace)
-                continue
-            lst.append(ch)
+    lst = []
+    for ch in my_list:
+        if ch == search:
+            lst.append(replace)
+            continue
+        lst.append(ch)
         return lst
