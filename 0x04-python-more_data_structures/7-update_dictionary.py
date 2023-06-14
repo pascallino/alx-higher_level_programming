52bbbfa9184b6121811ffb57727a28ac4bf3e58f
pascallino <luvpascal.ojukwu@yahoo.com>
2023-06-14 05:20:06 -0700

    dict update

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def update_dictionary(a_dictionary, key, value):
-    if a_dictionary:
+    if len(a_dictionary.keys()) > 0:
         if key in a_dictionary:
             a_dictionary[key] = value
             return a_dictionary
