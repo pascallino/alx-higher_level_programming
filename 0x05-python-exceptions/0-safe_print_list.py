00260c09b4f8483ffadd49ae3e0e4dc70fcd11e5
pascallino <luvpascal.ojukwu@yahoo.com>
2023-06-25 23:49:24 -0700

    readme

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+import sys
+
+
+def safe_print_list(my_list=[], x=0):
+    try:
+        count = 0
+        for i in range(0, x):
+            print("{}".format(my_list[i]), end="")
+            count += 1
+        print()
+    except IndexError:
+        print("", end='')
+    finally:
+        return count
