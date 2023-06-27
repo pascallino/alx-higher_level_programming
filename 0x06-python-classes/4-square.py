10d209ac1b06b285adec6cea4cd2e41f1f3d9a25
pascallino <luvpascal.ojukwu@yahoo.com>
2023-06-27 08:12:55 -0700

    4-suare

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Defines a class Square"""
+
+
+class Square:
+    """A Square Class"""
+    def __init__(self, size=0):
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        else:
+            if size < 0:
+                raise ValueError("size must be >= 0")
+            else:
+                self.__size = size
+
+    def area(self):
+        return self.__size * self.__size
+
+    def size(self):
+        return self.__size
+
+    def size(self, value):
+        self.__size = value
