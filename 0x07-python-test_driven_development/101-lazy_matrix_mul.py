3782f2aadc6c86d63ed99bcd7028da90c65d032b
pascallino <luvpascal.ojukwu@yahoo.com>
2023-07-01 03:19:14 -0700

    matrix

--- 0x07-python-test_driven_development/101-lazy_matrix_mul.py
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -11,5 +11,5 @@ def lazy_matrix_mul(m_a, m_b):
     """
     A1 = np.array(m_a)
     A2 = np.array(m_b)
-    # return (np.matmul(m_a, m_b))
-    return A1.dot(A2)
+    return (np.matmul(m_a, m_b))
+    # return A1.dot(A2)
