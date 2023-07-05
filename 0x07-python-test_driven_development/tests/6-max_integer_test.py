12d4eb979d78fb4439b97a11c27a7a61905ac7f4
pascallino <luvpascal.ojukwu@yahoo.com>
2023-07-05 07:40:51 +0100

    -max_integer_test.py

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -1,33 +1,18 @@
 #!/usr/bin/python3
-import unittest
-max_integer = __import__('6-max_integer').max_integer
+"""Module to find the max integer in a list
+"""
 
 
-class TestMaxInteger(unittest.TestCase):
-    def test_module_docstring(self):
-        """Tests for module docsting"""
-        m = __import__('6-max_integer').__doc__
-        self.assertTrue(len(m) > 1)
-
-    def test_function_docstring(self):
-        """Tests for funstion docstring"""
-        f = max_integer.__doc__
-        self.assertTrue(len(f) > 1)
-
-    """Unittest for max_integer([2, 7, 1, 100]))
+def max_integer(list=[]):
+    """Function to find and return the max integer in a list of integers
+        If the list is empty, the function returns None
     """
-    def test_max_interge_exact(self):
-        result = max_integer([2, 7, 1, 100])
-        self.assertEqual(result, 100)
-    def test_empty_list(self):
-        """Unittest for max_integer([..])
-        """
-        result = max_integer([])
-        self.assertEqual(result, None)
-    def test_no_arg(self):
-        """Unittest for no arguement"""
-        self.assertIsNone(max_integer())
-
-
-if __name__ == '__main__':
-    unittest.main()
+    if len(list) == 0:
+        return None
+    result = list[0]
+    i = 1
+    while i < len(list):
+        if list[i] > result:
+            result = list[i]
+        i += 1
+    return result
