95d68fd2303e144190beee7583519ac3dbae1b31
pascallino <luvpascal.ojukwu@yahoo.com>
2023-07-10 05:40:42 -0700

    geometr class

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+Contains the class BaseGeometry
+"""
+
+
+class BaseGeometry:
+    """An empty class"""
+    def def area(self):
+        """ Area method """
+        raise Exception("area() is not implemented")
