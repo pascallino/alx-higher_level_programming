10885253cf73c91ce35478d31cdaecd456622efc
pascallino <luvpascal.ojukwu@yahoo.com>
2023-07-13 10:50:48 +0100

    rectangle.py

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -102,7 +102,7 @@ class Rectangle(Base):
             str = str + '\n'
         print("{}".format(str), end="")
 
-    def update(self, *args):
+    def update(self, *args, **kwargs):
         """ update no key arguments """
         if args and len(args) > 0:
             a = 0
@@ -121,3 +121,19 @@ class Rectangle(Base):
                 elif a == 4:
                     self.y = arg
                 a += 1
+        elif kwargs and len(kwargs) != 0:
+            for k, v in kwargs.items():
+                if k == "id":
+                    if v is None:
+                        self.__init__(self.width, self.height, self.x, self.y)
+                    else:
+                        self.id = v
+                elif k == "width":
+                    self.width = v
+                elif k == "height":
+                    self.height = v
+                elif k == "x":
+                    self.x = v
+                elif k == "y":
+                    self.y = v
+
