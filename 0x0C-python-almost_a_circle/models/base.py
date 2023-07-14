af355b9ce17c5b580282da51812e72f41a0f5673
pascallino <luvpascal.ojukwu@yahoo.com>
2023-07-14 00:59:52 -0700

    base.py

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -34,7 +34,7 @@ class Base:
             else:
                 list_dicts = [o.to_dictionary() for o in list_objs]
                 jsonfile.write(cls.to_json_string(list_dicts))
-                
+
     @staticmethod
     def from_json_string(json_string):
         """Return the deserialization of a JSON string.
